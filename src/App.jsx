21dce5b25b5197152230f9ea16ab1bb199edb1c6
Yasir Hussain <59396511+YHZ-EPIC@users.noreply.github.com>
2023-08-25 22:13:42 +0500

    Post Deploy Key Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import Movie from "./Components/movie";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=YOUR_API_KEY";
+// const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=YOUR_API_KEY";
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=3b727a19";
 
 export default function App() {
   const [movie, setMovie] = useState([]);
