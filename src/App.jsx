5d5473ec9113b9b61450ecf440f5961e0ffe0d5a
Yasir Hussain <59396511+YHZ-EPIC@users.noreply.github.com>
2023-08-29 22:06:02 +0500

    Experimental
    Updated Many features + API

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,112 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import Movie from "./Components/movie";
 
-const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=3b727a19";
+// TMDB API
+const apiKey = import.meta.env.VITE_API_KEY;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&page=1`;
 
 export default function App() {
   const [movie, setMovie] = useState([]);
+  const [genres, setGenres] = useState({});
   const [searchTitle, setSearchTitle] = useState("");
+  const [section, setSection] = useState("Discover");
 
+  // Discover
   useEffect(() => {
-    SearchMovies("Thor");
+    axios
+      .get(API_URL)
+      .then((response) => {
+        console.log(response.data.results); // Log the fetched data
+        setMovie(response.data.results);
+      })
+      .catch((error) => console.error(error));
   }, []);
 
+  // Genre Fetch
+  useEffect(() => {
+    const genresUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
+
+    axios
+      .get(genresUrl)
+      .then((response) => {
+        const genresMap = {};
+        response.data.genres.forEach((genre) => {
+          genresMap[genre.id] = genre.name;
+        });
+        setGenres(genresMap);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  function formatWords(input) {
+    const words = input.split(" ");
+    const formattedWords = words.map((word) => {
+      const firstLetter = word.charAt(0).toUpperCase();
+      const restOfWord = word.slice(1).toLowerCase();
+      return firstLetter + restOfWord;
+    });
+    return formattedWords.join(" ");
+  }
+
   const SearchMovies = async (title) => {
-    await fetch(`${API_URL}&s=${title}`)
-      .then((response) => response.json())
-      .then((data) => setMovie(data.Search))
-      .catch((error) => console.error("\n --> Error Fetching Movies", error));
+    let formattedTitle = formatWords(title);
+    let displayTitle = formattedTitle.replace(/\s/g, "+");
+
+    setSection("Top Rated");
+
+    // Top - Rated
+    let Query = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
+
+    if (title) {
+      setSection(`Results for ${formattedTitle}`);
+
+      Query = `https://api.themoviedb.org/3/search/movie?query=${displayTitle}&api_key=${apiKey}`;
+    }
+
+    // console.log("\n Title : ", title);
+    // console.log("\n Formatted Title : ", formattedTitle);
+    // console.log("Search Query : ", Query);
+
+    axios
+      .get(Query)
+      .then((response) => {
+        console.log(response.data.results); // Log the fetched data
+        setMovie(response.data.results);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const fetchTrailerLinks = async (movieId) => {
+    const trailerUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&language=en-US`;
+
+    try {
+      const response = await axios.get(trailerUrl);
+      const videos = response.data.results;
+      const trailerLinks = videos
+        .filter((video) => video.type === "Trailer")
+        .map((video) => `https://www.youtube.com/watch?v=${video.key}`);
+
+      console.log(trailerLinks); // Array of trailer links
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <>
-      <h1 className="text-4xl font-bold text-center p-5">Movies Library</h1>
+      <h1 className="text-4xl font-bold text-center m-5 p-5">Movies Library</h1>
 
       <div className="flex justify-center items-center">
         <input
           type="text"
           value={searchTitle}
-          placeholder="Search Any Movie"
+          placeholder="Search For Movies"
+          onKeyDownCapture={(e) => {
+            if (e.key === "Enter") {
+              SearchMovies(searchTitle);
+            }
+          }}
           onChange={(e) => setSearchTitle(e.target.value)}
           className="border rounded-lg py-2 px-4 w-1/2 md:w-1/3 lg:w-1/4"
         />
@@ -40,19 +120,19 @@ export default function App() {
         </button>
       </div>
 
-      {/* Render All Movie Cards using Map Function */}
-      {movie?.length > 0 ? (
-        movie.map((movie) => (
-          <li key={movie.imdbID} className="list-none">
-            <Movie myData={movie} />
-          </li>
-        ))
-      ) : (
-        <h1 className="text-3xl font-bold text-center p-5">
-          {" "}
-          No Movies Found{" "}
-        </h1>
-      )}
+      {/* <span>
+        <p>Genres: {movie.genre_ids.map(id => genres[id]).join(', ')}</p>
+      </span> */}
+
+      <h2 className="text-4xl font-bold text-center p-4 pb-0 m-4 mb-0 text-orange-500">
+        {section}
+      </h2>
+
+      {movie.map((movie) => (
+        <li key={movie.id} className="list-none">
+          <Movie myData={movie} genres={genres} trailer = {fetchTrailerLinks(movie.id)} />
+        </li>
+      ))}
     </>
   );
 }
