14ded84ec5c0bac59c3b530c06a9da3e3fd0bd4e
Yasir Hussain <59396511+YHZ-EPIC@users.noreply.github.com>
2024-08-18 22:11:37 +0500

    Added About Me Button
    Added Icons, Added About Me in Navbar to Redirect to my Portfolio Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,73 @@
-import Home from "./Components/Home";
-import Error from "./Components/Error";
-import Navbar from "./Components/Navbar";
-import WatchList from "./Components/WatchList";
-import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import Home from './Components/Home';
+import Error from './Components/Error';
+import Navbar from './Components/Navbar';
+import WatchList from './Components/WatchList';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 // TMDB API
 const apiKey = import.meta.env.VITE_API_KEY;
 
 function About() {
-  return <h2>About Page</h2>;
-}
-
-function Contact() {
-  return <h2>Contact Page</h2>;
+	return <h2>About Page</h2>;
 }
 
 export default function App() {
-  const [watchList, setWatchList] = useState([]);
+	const [watchList, setWatchList] = useState([]);
 
-  const handleAddToWatchList = (item) => {
-    // Check if the item already exists in the watchList
-    const itemExists = watchList.some(
-      (existingItem) => existingItem.id === item.id
-    );
+	const handleAddToWatchList = (item) => {
+		// Check if the item already exists in the watchList
+		const itemExists = watchList.some(
+			(existingItem) => existingItem.id === item.id
+		);
 
-    if (!itemExists) {
-      // If it doesn't exist, add it to the watchList
-      setWatchList([...watchList, item]);
-    } else {
-      // Handle duplicate items
-      console.log("\n --> " + item.title + " is already in the Watch List.");
-    }
-  };
+		if (!itemExists) {
+			// If it doesn't exist, add it to the watchList
+			setWatchList([...watchList, item]);
+		} else {
+			// Handle duplicate items
+			console.log('\n --> ' + item.title + ' is already in the Watch List.');
+		}
+	};
 
-  const updateList = (updatedItems) => {
-    setWatchList(updatedItems);
-  };
+	const updateList = (updatedItems) => {
+		setWatchList(updatedItems);
+	};
 
-  return (
-    <>
-      <div className="container mx-auto p-4">
-        <Navbar />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <Home apiKey={apiKey} sendDataToParent={handleAddToWatchList} />
-            }
-          />
-          <Route
-            path="/list"
-            element={<WatchList favorites={watchList} onChange={updateList} />}
-          />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='container p-4 mx-auto'>
+				<Navbar />
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={
+							<Home
+								apiKey={apiKey}
+								sendDataToParent={handleAddToWatchList}
+							/>
+						}
+					/>
+					<Route
+						path='/list'
+						element={
+							<WatchList
+								favorites={watchList}
+								onChange={updateList}
+							/>
+						}
+					/>
+					<Route
+						path='/about'
+						element={<About />}
+					/>
+					<Route
+						path='*'
+						element={<Error />}
+					/>
+				</Routes>
+			</div>
+		</>
+	);
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,39 +1,40 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
-  return (
-    <nav className="text-xl p-1.5 m-0.5 font-semibold text-white">
-      <div className="container mx-auto flex justify-between items-center">
-        <NavLink to="/">
-          <img
-            alt="Home"
-            title="Home"
-            src="./vite.svg"
-            className="w-12 h-12 mb-1"
-          />
-        </NavLink>
+	return (
+		<nav className='text-xl p-1.5 m-0.5 font-semibold text-white'>
+			<div className='container flex items-center justify-between mx-auto'>
+				<NavLink to='/'>
+					<img
+						alt='Home'
+						title='Home'
+						src='../../public/cinema.png'
+						className='rounded-lg hover:border-2 h-14 w-14 hover:border-spacing-2 hover:bg-gray-300'
+					/>
+				</NavLink>
 
-        <div className="space-x-4">
-          <NavLink
-            to="/"
-            className="text-white hover:text-gray-300"
-            activeclassname="font-bold"
-          >
-            Home
-          </NavLink>
+				<div className='space-x-4'>
+					<NavLink
+						to='/'
+						className='text-white hover:text-gray-300'
+						activeclassname='font-bold'>
+						<button className='px-4 py-2 text-white'> 🏠 Home</button>
+					</NavLink>
+					<NavLink
+						to='https://my-portfolio-yhz.vercel.app/'
+						target='_blank'
+						className='border-purple-500 border-3'
+						activeclassname='font-bold'>
+						<button className='px-4 py-2 text-white'> 👉 About Me</button>
+					</NavLink>
 
-          <NavLink to="/list" className="hover:text-gray-300">
-            Watch List
-          </NavLink>
-
-          {/* <NavLink to="/about" className=" hover:text-gray-300">
-            About
-          </NavLink>
-          <NavLink to="/contact" className=" hover:text-gray-300">
-            Contact
-          </NavLink> */}
-        </div>
-      </div>
-    </nav>
-  );
+					<NavLink
+						to='/list'
+						className='hover:text-gray-300'>
+						<button className='px-4 py-2 text-white'> 📃 Watch List</button>
+					</NavLink>
+				</div>
+			</div>
+		</nav>
+	);
 }
