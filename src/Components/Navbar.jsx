09b957691da35dc252389808b0396fbc55470634
Yasir Hussain <59396511+YHZ-EPIC@users.noreply.github.com>
2024-08-29 20:26:03 +0500

    Updated Navbar to be Responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,15 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Navbar() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
-		<nav className='text-xl p-1.5 m-0.5 font-semibold text-white'>
+		<nav className='p-4 '>
 			<div className='container flex items-center justify-between mx-auto'>
 				<NavLink to='/'>
 					<img
@@ -13,21 +20,41 @@ export default function Navbar() {
 					/>
 				</NavLink>
 
-				<div className='space-x-4'>
+				<div className='md:hidden'>
+					<button
+						onClick={toggleMenu}
+						className='text-white focus:outline-none'>
+						{/* Hamburger Icon */}
+						<svg
+							className='w-6 h-6'
+							fill='none'
+							stroke='currentColor'
+							viewBox='0 0 24 24'
+							xmlns='http://www.w3.org/2000/svg'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M4 6h16M4 12h16m-7 6h7'
+							/>
+						</svg>
+					</button>
+				</div>
+
+				<div className='hidden space-x-4 md:flex'>
 					<NavLink
 						to='/'
 						className='text-white hover:text-gray-300'
 						activeclassname='font-bold'>
-						<button className='px-4 py-2 text-white'> 🏠 Home</button>
+						<button className='px-4 py-2'> 🏠 Home</button>
 					</NavLink>
 					<NavLink
 						to='https://my-portfolio-yhz.vercel.app/'
 						target='_blank'
 						className='border-purple-500 border-3'
 						activeclassname='font-bold'>
-						<button className='px-4 py-2 text-white'> 👉 About Me</button>
+						<button className='px-4 py-2'> 👉 About Me</button>
 					</NavLink>
-
 					<NavLink
 						to='/list'
 						className='hover:text-gray-300'>
@@ -35,6 +62,33 @@ export default function Navbar() {
 					</NavLink>
 				</div>
 			</div>
+
+			{/* Mobile menu */}
+			{isOpen && (
+				<div className='flex flex-col items-center mt-4 space-y-4 md:hidden'>
+					<NavLink
+						to='/'
+						className='text-white hover:text-gray-300'
+						activeclassname='font-bold'>
+						<button className='w-full px-4 py-2'> 🏠 Home</button>
+					</NavLink>
+					<NavLink
+						to='https://my-portfolio-yhz.vercel.app/'
+						target='_blank'
+						className='border-purple-500 border-3'
+						activeclassname='font-bold'>
+						<button className='w-full px-4 py-2'> 👉 About Me</button>
+					</NavLink>
+					<NavLink
+						to='/list'
+						className='hover:text-gray-300'>
+						<button className='w-full px-4 py-2 text-white'>
+							{' '}
+							📃 Watch List
+						</button>
+					</NavLink>
+				</div>
+			)}
 		</nav>
 	);
 }
